6f93b956e9427895e70e00cdc8d6bb7a1ab63037
altafziyaa <altafziyaa91@gmail.com>
2026-02-03 16:13:51 +0530

    new update

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import express from "express";
 import allService from "./service/allService.js";
 const app = express();
 app.use(express.json());
+// app.use(body.parser());
 
 app.use("/api/users", allService);
 connectDB();
 app.listen(3000, () => {
-  console.log("API Gateway running on port 3000");
+  console.log("server running on port 3000");
 });
--- service/allService.js
+++ service/allService.js
@@ -3,9 +3,17 @@ import userModel from "./userModel.js";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
+  const { name, email, age } = req.body;
   try {
-    const user = userModel.create(req.body);
-    res.status(200).json(user);
+    const user = await userModel.create({ name, email, age });
+    res.status(201).json(user);
+
+    // return res.status(201).json({
+    //   success: true,
+    //   message: "user created",
+    //   user,
+    // });
+    // await user.save();
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
